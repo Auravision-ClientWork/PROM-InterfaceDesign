10c7a26c94e7ee2a99ea72794abc2d29fd416666
Auravision <auravision.africa@gmail.com>
2022-07-03 11:09:36 +0300

    UI Flow framework in place. Moving on to creating region score panels.

--- Assets/_Aura/Scripts/UIFramework/AuraUIScreen.cs
+++ Assets/_Aura/Scripts/UIFramework/AuraUIScreen.cs
@@ -1,11 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Aura
 {
+    [RequireComponent(typeof(CanvasGroup))]
+    [RequireComponent(typeof(Animator))]
     public class AuraUIScreen : MonoBehaviour
     {
-     
-    } 
+        Animator animator;
+
+        public Selectable startSelectable;
+
+        public UnityEvent OnStartScreen = new UnityEvent();
+        public UnityEvent OnCloseScreen = new UnityEvent();
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            gameObject.SetActive(false);
+        }
+
+        public void StartScreen()
+        {
+            EventSystem.current.SetSelectedGameObject(startSelectable.gameObject);
+
+            OnStartScreen?.Invoke();
+
+            HandleAnimation("show");
+        }
+
+        public void CloseScreen()
+        {
+            Debug.Log("inside CloseScreen()");
+            gameObject.SetActive(false);
+            OnCloseScreen?.Invoke();
+            HandleAnimation("hide");
+        }
+
+        private void HandleAnimation(string aTrigger)
+        {
+            animator.SetTrigger(aTrigger);
+        }
+    }
 }
--- Assets/_Aura/Scripts/UIFramework/AuraUISystem.cs
+++ Assets/_Aura/Scripts/UIFramework/AuraUISystem.cs
@@ -1,3 +1,4 @@
+using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,54 @@ namespace Aura
 {
     public class AuraUISystem : MonoBehaviour
     {
-       
+        //reference to all the screens
+        public AuraUIScreen[] screens;
+        public AuraUIScreen firstScreen;
+        //reference to the current screen
+        private AuraUIScreen currentScreen;
+        //reference to the previous screen
+        private AuraUIScreen previousScreen;
+        //reference to the first screen
+
+        public UnityEvent OnSwitchScreens = new UnityEvent();
+
+        private void Start()
+        {
+            screens = GetComponentsInChildren<AuraUIScreen>(true);
+         
+            SwitchScreens(firstScreen);
+        }
+
+        public void SwitchScreens(AuraUIScreen aScreen)
+        {
+            if (currentScreen)
+            {
+                //close it
+                currentScreen.CloseScreen();
+                //make it the previous screen
+                previousScreen = currentScreen;
+            }
+            currentScreen = aScreen;
+            currentScreen.gameObject.SetActive(true);
+            //call the start method on it
+            //let interested listeners know that we have switched screens
+            OnSwitchScreens?.Invoke();
+        }
+
+        public void GoToPreviousScreen()
+        {
+            Debug.Log("in Go to previous screen");
+            if(previousScreen)
+            SwitchScreens(previousScreen);
+        }
+
+        public void TurnOffAllScreens()
+        {
+            Debug.Log("inside TurnOffAllScreens");
+            foreach (var aScreen in screens)
+            {
+                aScreen.gameObject.SetActive(false);
+            }
+        }
     } 
 }
